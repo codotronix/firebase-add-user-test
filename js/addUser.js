493a7dfc491c385deb94b0ce5b2e718ca89c2887
codotronix <codotronix@gmail.com>
2015-06-04 01:46:25 +0530

    some upgrades

--- js/addUser.js
+++ js/addUser.js
@@ -2,7 +2,29 @@ $(function(){
 	'use strict'
 	var userRef = new Firebase('https://crackling-inferno-4598.firebaseio.com/user');
 
-	$('#btnAddUser').click(function(){
+	$('#btnAddUser').click(function () {
+		if (allReqFilled()) {
+			console.log('all fields filled up ...');
+			//addUserToDb();			
+			$('#msg').text('Success...').removeClass('error').addClass('success');
+		}
+		else {
+			$('#msg').text('All fields are mandatory...').removeClass('success').addClass('error');
+		}	
+	});
+
+	function allReqFilled () {
+		var r = true;
+		$('.required').each(function () {
+			if ($(this).val().length == 0) {
+				$(this).addClass('error');
+				r = false;
+			}
+		})
+		return r;
+	}
+
+	function addUserToDb () {
 		var user = {};
 		user.name = $('#nameOfUser').val();
 		user.country = $('#country').val();
@@ -12,7 +34,6 @@ $(function(){
 		
 		var newPostRef = userRef.push(user, function(){
 			console.log('push successful... key=' + newPostRef.key());
-		})
-
-	});
+		});
+	}
 })
